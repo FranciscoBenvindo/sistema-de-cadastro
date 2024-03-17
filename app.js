873e6ab5c4badf019a8c69cfbfbde8b8767c1b24
FranciscoBenvindo <fbenvindo71@gmail.com>
2024-03-17 19:26:33 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+//mais console log de teste:
+console.log("O paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o Github")
+
